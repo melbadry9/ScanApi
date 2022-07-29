6e9d312bd044bd2421fc5088bf3c30c0b34bf875
Mohamed Elbadry <mohamed.sa.elbadry@gmail.com>
2022-07-29 08:12:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,29 +13,24 @@ export PATH=$PATH:$GOPATH/bin;" >> ~/.profile;
 source ~/.profile;
 
 # Install Newer Version of golang
-go get golang.org/dl/go1.13;
-go get golang.org/dl/go1.16;
-go1.13 download;
-go1.16 download;
+go get golang.org/dl/go1.17;
+go1.17 download;
 
-# Installing tools
-go get -v github.com/melbadry9/enumsho;
-go1.16 get -v github.com/cgboal/sonarsearch/crobat;
-go1.16 get -v github.com/tomnomnom/assetfinder;
-go1.16 get -v github.com/tomnomnom/httprobe;
-go1.16 get -v github.com/melbadry9/subover;
-go1.16 install github.com/OJ/gobuster/v3@latest;
-GO111MODULE=on go1.16 get -v github.com/projectdiscovery/httpx/cmd/httpx
-GO111MODULE=on go1.16 get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei;
-GO111MODULE=on go1.16 get -v github.com/projectdiscovery/chaos-client/cmd/chaos;
-GO111MODULE=on go1.16 get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder;
+go get -v github.com/melbadry9/enumsho
+go1.17 get -v github.com/cgboal/sonarsearch/cmd/crobat
+go1.17 install -v github.com/tomnomnom/httprobe@latest
+go1.17 install -v github.com/tomnomnom/assetfinder@latest
+go1.17 install -v github.com/melbadry9/subover@latest
+go1.17 install -v github.com/OJ/gobuster/v3@latest
+go1.17 install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+go1.17 install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go1.17 install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go1.17 install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest
 
-# Amass
-wget --quiet https://github.com/OWASP/Amass/releases/download/v3.13.2/amass_linux_amd64.zip;
-unzip -j amass_linux_amd64.zip amass_linux_amd64/amass -d ${GOPATH}/bin/;
-rm amass_linux_amd64.zip;
+wget --quiet https://github.com/Edu4rdSHL/findomain/releases/latest/download/findomain-linux
+chmod +x findomain-linux
+mv findomain-linux ${GOPATH}/bin/findomain
 
-# Findomain
-wget --quiet https://github.com/Edu4rdSHL/findomain/releases/latest/download/findomain-linux;
-chmod +x findomain-linux;
-mv findomain-linux ${GOPATH}/bin/findomain;
+wget --quiet https://github.com/OWASP/Amass/releases/download/v3.19.3/amass_linux_amd64.zip
+unzip -j amass_linux_amd64.zip amass_linux_amd64/amass -d ${GOPATH}/bin/
+rm amass_linux_amd64.zip
